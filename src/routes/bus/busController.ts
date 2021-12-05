4b23122eb891b3545b49e4fc6368f5622e3b8259
Swaraj Deep <deep.swaraj7@gmail.com>
2021-12-05 16:09:42 +0530

    Add filter in bus routes

--- src/routes/bus/busController.ts
+++ src/routes/bus/busController.ts
@@ -3,10 +3,12 @@ import {getAllBuses, getSingleBus} from './getBuses';
 import {postBus} from "./postBus";
 import {updateBus} from "./updateBus";
 import {deleteBus} from "./deleteBus";
+import {getBetweenLocation} from "./getBetweenLocation";
 
 const router: Router = express.Router();
 router.use(express.json());
 
+router.get('/getBetweenLocation', getBetweenLocation);
 router.get("/", getAllBuses);
 router.get("/:id", getSingleBus);
 router.post("/newBus", postBus);
---
+++ src/routes/bus/getBetweenLocation.ts
@@ -0,0 +1,26 @@
+import {Request, Response, NextFunction} from "express";
+import {Connection} from "mongoose";
+import connectToDB from "../../database/connectToDB";
+import ErrorHandler from "../../errorHandling/errorHandler";
+import {IBus} from "../../models/IBus";
+import viewWithFilter from "../../shared/viewWithFilter";
+import BusSchema from "../../models/BusSchema";
+import sendResponse from "../../shared/sendResponse";
+
+export async function getBetweenLocation(req: Request, res: Response, next: NextFunction) {
+    let dbConnection!: Connection;
+    try {
+        dbConnection = connectToDB(process.env.CONNECTION_STRING!);
+        const to: string = req.query.to as string;
+        const from: string = req.query.from as string;
+        const buses: IBus[] = await viewWithFilter<IBus>(dbConnection, 'Bus', BusSchema, {
+            to,
+            from
+        });
+        sendResponse(res, 200, buses);
+    } catch (err: any) {
+        next(new ErrorHandler(500, err.message));
+    } finally {
+        await dbConnection.close();
+    }
+}
