78a5e2fc8c9b87f6850d1a8c660fd23d13405e91
Swaraj Deep <deep.swaraj7@gmail.com>
2021-12-07 16:19:10 +0530

    Add code for fetching remaining seats

--- src/routes/bus/busController.ts
+++ src/routes/bus/busController.ts
@@ -4,6 +4,7 @@ import {postBus} from "./postBus";
 import {updateBus} from "./updateBus";
 import {deleteBus} from "./deleteBus";
 import {getBetweenLocation} from "./getBetweenLocation";
+import {getRemainingSeats} from "./getRemainingSeats";
 
 const router: Router = express.Router();
 router.use(express.json());
@@ -11,6 +12,7 @@ router.use(express.json());
 router.get('/getBetweenLocation', getBetweenLocation);
 router.get("/", getAllBuses);
 router.get("/:id", getSingleBus);
+router.get('/getRemainingSeats/:id', getRemainingSeats);
 router.post("/newBus", postBus);
 router.patch("/updateBus/:id", updateBus);
 router.delete("/deleteBus/:id", deleteBus);
---
+++ src/routes/bus/getRemainingSeats.ts
@@ -0,0 +1,34 @@
+import {Request, Response, NextFunction} from "express";
+import {Connection} from "mongoose";
+import ErrorHandler from "../../errorHandling/errorHandler";
+import connectToDB from "../../database/connectToDB";
+import viewWithFilter from "../../shared/viewWithFilter";
+import {IBus} from "../../models/IBus";
+import BusSchema from "../../models/BusSchema";
+import sendResponse from "../../shared/sendResponse";
+
+export async function getRemainingSeats(req: Request, res: Response, next: NextFunction) {
+    let dbConnection!: Connection;
+    try {
+        const id = req.params.id;
+        dbConnection = connectToDB(process.env.CONNECTION_STRING!);
+        const remainingSeats: IBus[] = await viewWithFilter<IBus>(dbConnection, 'Bus', BusSchema, {
+            _id: id,
+        }, {
+            _id: false,
+            companyId: false,
+            to: false,
+            from: false,
+            busType: false,
+            timings: false,
+            fare: false,
+            totalSeats: false,
+            remainingSeats: true
+        });
+        sendResponse(res, 200, remainingSeats);
+    } catch (err: any) {
+        next(new ErrorHandler(500, err.message));
+    } finally {
+        await dbConnection.close();
+    }
+}
